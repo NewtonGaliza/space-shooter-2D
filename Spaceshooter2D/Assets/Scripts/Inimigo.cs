f54d86c224b20a302ca1013067126e565a3a7ad8
Newton Galiza <newtonjgaliza@gmail.com>
2022-11-18 16:07:14 -0300

    part 11 & 12
    part 11 & 12

--- Spaceshooter2D/Assets/Scripts/Inimigo.cs
+++ Spaceshooter2D/Assets/Scripts/Inimigo.cs
@@ -10,6 +10,8 @@ public class Inimigo : MonoBehaviour
 
     private float velocidadeY;
 
+    public ParticleSystem particulaExplosaoPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +21,19 @@ public class Inimigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //como inimigo sé se movimenta de cima para baixo, o valor X é zerado
+        //como inimigo só se movimenta de cima para baixo, o valor X é zerado
         //negativando a velocidadeY para o inimiog se moverr para baixo
         this.rigidbody.velocity = new Vector2(0, -this.velocidadeY);
+
+        //Camera.main retorna a camera principal do jogo
+        Camera camera = Camera.main;
+        Vector3 posicaoNaCamera = camera.WorldToViewportPoint(this.transform.position);
+        if(posicaoNaCamera.y < 0)
+        {
+            NaveJogador jogador = GameObject.FindGameObjectWithTag("Player").GetComponent<NaveJogador>();
+            jogador.Vida--;
+            Destruir(false);
+        }
     }
 
     public void Destruir(bool derrotado)
@@ -30,6 +42,10 @@ public class Inimigo : MonoBehaviour
         {
             ControladorPontuacao.Pontuacao++;
         }
+
+        ParticleSystem particulaExplosao = Instantiate(this.particulaExplosaoPrefab, this.transform.position, Quaternion.identity);
+        Destroy(particulaExplosao.gameObject, 1f); //destroi a particula aops 1 segundo
+
         Destroy(this.gameObject);
     }
 }
