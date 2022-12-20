e39889abfb05680a27b31e8d4e6d1c7c9ea5b620
Newton Galiza <newtonjgaliza@gmail.com>
2022-12-20 18:12:16 -0300

    part 15
    part 15

--- Spaceshooter2D/Assets/Scripts/NaveJogador.cs
+++ Spaceshooter2D/Assets/Scripts/NaveJogador.cs
@@ -1,4 +1,4 @@
-  using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,8 @@ public class NaveJogador : MonoBehaviour
     [SerializeField] private float tempoEsperaTiro;
 
     [SerializeField] private int vidas;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public Transform[] posicoesArmas;
     private Transform armaAtual;
@@ -48,8 +50,9 @@ public class NaveJogador : MonoBehaviour
             //reseta a contgem de tempo
             this.intervaloTiro = 0;
             Atirar();
-
         }
+
+        VerificarLimiteTela();   
     }
 
     private void Atirar()
@@ -92,6 +95,65 @@ public class NaveJogador : MonoBehaviour
             Vida--;
             Inimigo inimigo = collider.GetComponent<Inimigo>();
             inimigo.ReceberDano();
+        }
+    }
+
+    private void VerificarLimiteTela()
+    {
+        Vector2 posicaoAtual = this.transform.position;
+
+        float metadeLargura = Largura / 2f;
+        float metadeAltura = Altura / 2f;
+
+        Camera camera = Camera.main;
+        Vector2 limiteInferiorEsquerdo = camera.ViewportToWorldPoint(Vector2.zero); // (0, 0) no plano cartesiano
+        Vector2 limiteSuperiorDireito = camera.ViewportToWorldPoint(Vector2.one); // (1, 1) no plano cartesiano
+
+        float pontoReferenciaEsquerdo = posicaoAtual.x - metadeLargura;
+        float pontoReferenciaDireito = posicaoAtual.x + metadeLargura;
+
+        //saindo pela esquerda
+        if (pontoReferenciaEsquerdo < limiteInferiorEsquerdo.x)
+        {
+            this.transform.position = new Vector2(limiteInferiorEsquerdo.x + metadeLargura, posicaoAtual.y);
+        } else if (pontoReferenciaDireito > limiteSuperiorDireito.x) //saindo pela direita
+        {
+            this.transform.position = new Vector2(limiteSuperiorDireito.x - metadeLargura, posicaoAtual.y);
+        }
+
+        // nao permitir que o jogador saia da tela pelos cantos diagonais
+        posicaoAtual = this.transform.position;
+
+        float pontoReferenciaSuperior = posicaoAtual.y + metadeAltura;
+        float pontoReferenciaInferior = posicaoAtual.y - metadeAltura;
+
+        //saindo por cima
+        if (pontoReferenciaSuperior > limiteSuperiorDireito.y)
+        {
+            this.transform.position = new Vector2(posicaoAtual.x, limiteSuperiorDireito.y - metadeAltura);
+        } else if (pontoReferenciaInferior < limiteInferiorEsquerdo.y) //saindo por baixo
+        {
+            this.transform.position = new Vector2(posicaoAtual.x, limiteInferiorEsquerdo.y + metadeAltura);
+        }
+    }
+
+    private float Largura
+    {
+        get
+        {
+            Bounds bounds = this.spriteRenderer.bounds;
+            Vector3 tamanho = bounds.size;
+            return tamanho.x;
+        }
+    }
+
+    private float Altura
+    {
+        get
+        {
+            Bounds bounds = this.spriteRenderer.bounds;
+            Vector3 tamanho = bounds.size;
+            return tamanho.y;
         }
     }
 }
