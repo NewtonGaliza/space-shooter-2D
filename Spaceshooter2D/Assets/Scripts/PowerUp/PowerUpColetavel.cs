dd1afe52c8d88b9885127eae98edc4e02f207362
Newton Galiza <newtonjgaliza@gmail.com>
2023-01-16 15:21:54 -0300

    part 24
    part 24

--- Spaceshooter2D/Assets/Scripts/PowerUp/PowerUpColetavel.cs
+++ Spaceshooter2D/Assets/Scripts/PowerUp/PowerUpColetavel.cs
@@ -1,15 +1,63 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PowerUpColetavel : MonoBehaviour
 {
-     
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float intervaloTempoAntesAutodestruir;
+    private float contagemTempoAntesUtodestruir;     
+    private bool autoDestruindo;
+    [SerializeField] private float intervaloTempoEntrePiscadas;
+    [SerializeField] private int quantidadeTotalPiscadas;
+    [SerializeField] private float reducaoTempoEntrePiscadas;
+
     public abstract EfeitoPowerUp EfeitoPowerUp { get; }
+
+    private void Start()
+    {
+        this.contagemTempoAntesUtodestruir = 0;  
+        this.autoDestruindo = false;      
+    }
+
+    private void Update()
+    {
+        if(!this.autoDestruindo)
+        {
+            this.contagemTempoAntesUtodestruir += Time.deltaTime;        
+            if(this.contagemTempoAntesUtodestruir >= this.intervaloTempoAntesAutodestruir)
+            {
+                IniciarAutoDestricao();
+            }
+        }
+    }
 
     public void Coletar()
     {
         Destroy(this.gameObject);
     }
+
+    public void IniciarAutoDestricao()
+    {
+        this.autoDestruindo = true;
+       
+        StartCoroutine(AutoDestruir());
+    }
+
+    private IEnumerator AutoDestruir()
+    {
+        int contadorPiscadas = 0;
+        do
+        {
+            this.spriteRenderer.enabled = !this.spriteRenderer.enabled;
+            contadorPiscadas++;
+            yield return new WaitForSeconds(this.intervaloTempoEntrePiscadas);
+            this.intervaloTempoEntrePiscadas -= contadorPiscadas * this.reducaoTempoEntrePiscadas;            
+        }
+        while(contadorPiscadas < this.quantidadeTotalPiscadas);
+
+        Destroy(this.gameObject);
+    }
+
 
 }
